6e25533b136fc28c7eb9ad5927d49501926d6758
Gustavo-Ferreira02 <119418690+Gustavo-Ferreira02@users.noreply.github.com>
2022-12-01 01:33:43 -0300

    Create Mao_Jogador.cpp

---
+++ Mao_Jogador.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Mao_Jogador.hpp"
+
+Mao_Joagador::Mao_Joagador()
+{}
+void Mao_Joagador::addCartas(string carta){
+cartas.push_back(carta);
+}
