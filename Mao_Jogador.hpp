6bb0490f395ecf839f9037ffaf184eaf19b1dbb4
Gustavo-Ferreira02 <119418690+Gustavo-Ferreira02@users.noreply.github.com>
2022-11-30 22:56:46 -0300

    Create Mao_Jogador.hpp

---
+++ Mao_Jogador.hpp
@@ -0,0 +1,15 @@
+#ifndef MAO_JOGADOR_H
+#define MAO_JOGADOR_H
+
+   class Mao_Joagador
+{
+private:
+    /* data */
+public:
+    vector<string> cartas;
+    Mao_Joagador();
+
+    void addCartas(string carta );
+};
+
+#endif
